ad92176b674ec8b004ab0c5cd70375e5b954c35c
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-05-01 16:32:42 -0300

    refatorado: adicionado crossorigin

--- src/main/java/com/joaogabriel/cotacao_moedas/resources/CotacaoResource.java
+++ src/main/java/com/joaogabriel/cotacao_moedas/resources/CotacaoResource.java
@@ -3,12 +3,14 @@ package com.joaogabriel.cotacao_moedas.resources;
 import java.math.BigDecimal;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cotacao")
 public class CotacaoResource {
