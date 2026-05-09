5198e647426235d56aae8fbc2c8184b028667e7f
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-05-08 21:25:04 -0300

    feat: create health resource

---
+++ src/main/java/com/joaogabriel/cotacao_moedas/resources/HealthResource.java
@@ -0,0 +1,14 @@
+package com.joaogabriel.cotacao_moedas.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthResource {
+
+	@GetMapping
+	public ResponseEntity<String> health(){
+		return ResponseEntity.ok("ok");
+	}
+}
