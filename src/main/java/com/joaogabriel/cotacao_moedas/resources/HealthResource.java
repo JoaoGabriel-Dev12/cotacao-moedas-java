8d791cd228c0881d797e50088bbd3aa7405e5af0
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-05-08 21:32:20 -0300

    updated: rote for health method

--- src/main/java/com/joaogabriel/cotacao_moedas/resources/HealthResource.java
+++ src/main/java/com/joaogabriel/cotacao_moedas/resources/HealthResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthResource {
 
-	@GetMapping
+	@GetMapping("/health")
 	public ResponseEntity<String> health(){
 		return ResponseEntity.ok("ok");
 	}
